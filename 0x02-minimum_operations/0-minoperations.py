569e44ed5b6304eb9ada1395381b07e3a6e9d5a0
elcodo254 <ellawachaga@gmail.com>
2023-08-13 21:07:46 +0000

    fix pep8 styling errors

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-Given a number n, write a method that calcuates the fewest number of operations needed to result in exactly n H(single character in text file) characters in the file.
+Given a number n, write a method that calcuates the fewest number of
+/operations needed to result in exactly n H(single character in text file)
+/characters in the file.
 """
 
 
@@ -12,7 +14,7 @@ def minOperations(n):
     if n <= 1:
         return 0
     for i in range(2, n + 1):
-        while n% i == 0:
+        while n % i == 0:
             n = n / i
             operations += i
     return operations
